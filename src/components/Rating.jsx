78f2e7913a622a0f6d1f1bbe38dc592c6879c1dc
Lionel <nkabisto@gmail.com>
2026-02-02 15:55:02 +0200

    Replaced inline css styles with one global css file named index.css

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,19 +1,9 @@
 const Rating = () => {
   return (
-    <div style={styles.container}>
-      <h2 style={styles.heading}>Rate Your Experience</h2>
+    <div className="rating-container">
+      <h2>Rate Your Experience</h2>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    margin: 0,
-    textAlign: "center",
-    fontFamily: "Arial",
-    padding: "20px",
-  },
-  heading: { color: "red" },
 };
 
 export default Rating;
