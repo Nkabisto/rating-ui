a38296458108bd608483e3ab89a4f47511b565dd
Lionel <nkabisto@gmail.com>
2026-02-04 14:49:23 +0200

    Experimented with event handling events: onClick, onMouseEnter and onMouseLeave

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,21 @@
 const Rating = () => {
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
+  const clicked = (index) => console.log("Clicked!", index);
+  const hovered = (direction) => console.log("Hovered!", direction);
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
         {stars.map((star) => (
-          <span key={star} className="star">
+          <span
+            onClick={() => clicked(star)}
+            onMouseEnter={() => hovered("enter")}
+            onMouseLeave={() => hovered("leave")}
+            key={star}
+            className="star"
+          >
             {"\u2605"}
           </span>
         ))}
